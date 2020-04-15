cb53104faefa7fc125ff12dc2217bc40dc9e1dc4
angry_beard <better_river@163.com>
2020-04-16 00:09:53 +0800

    单例

--- design-mode/src/main/java/com/beard/train/singleton/lazy/LazyThread.java
+++ design-mode/src/main/java/com/beard/train/singleton/lazy/LazyThread.java
@@ -2,7 +2,6 @@ package com.beard.train.singleton.lazy;
 
 public class LazyThread implements Runnable {
 
-    @Override
     public void run() {
         LazySimpleSingleton singleton = LazySimpleSingleton.getInstance();
         System.out.println(Thread.currentThread().getName() + ":" + singleton);
